fe129466bfbe3c97f337b9e3fb1b7a9bbcd9072d
sandvik16 <sannndvik16@gmail.com>
2022-04-18 11:40:29 +0300

    feat: change ormconfig for heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,17 +17,17 @@ import { CommentModule } from 'src/comment/comment.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot(
-      ormconfing
-      // {
-      //   url: process.env.DATABASE_URL,
-      //   type: 'postgres',
-      //   ssl: {
-      //     rejectUnauthorized: false,
-      //   },
-      //   entities: ['dist/**/*.entity{.ts,.js}'],
-      //   synchronize: true, // This for development
-      //   autoLoadEntities: true,
-      // },
+      // ormconfing
+      {
+        url: process.env.DATABASE_URL,
+        type: 'postgres',
+        ssl: {
+          rejectUnauthorized: false,
+        },
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        synchronize: true, // This for development
+        autoLoadEntities: true,
+      },
     ),
     UserModule,
     ColumnModule,
